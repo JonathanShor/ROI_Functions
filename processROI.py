248044bd6afa94d0a37e6ec4e9be710c9e134b42
Jonathan Shor <jds814@nyu.edu>
2019-06-19 18:30:06 -0400

    Adds frame trigger dataset cleaning methods.
    Frame trigger recording has at least two sources of error.
    processROI.clean_frame_trigger_data now corrects the two known.
    
    Also added a clean boolean parameter to
    processROI.get_flatten_trial_data.
    Currently defaults to False so it gives the raw data.

--- processROI.py
+++ processROI.py
@@ -110,11 +110,19 @@ def open_TIFF_stack(tiffsPattern="*.tif*"):
 
 
 def get_trials_metadata(h5Filename):
+    """Retrieve "Trials" metadata from an h5 file.
+
+    Arguments:
+        h5Filename {str} -- Source filepath
+
+    Returns:
+        pd.DataFrame -- Each DataFrame label corresponds to a h5 dataset.
+    """
     trialsMeta = pd.read_hdf(h5Filename, key="Trials")
     return trialsMeta
 
 
-def get_flatten_trial_data(h5Filename, key):
+def get_flatten_trial_data(h5Filename, key, clean=False):
     """Extract data for key from all Trials in h5Filename.
 
     Arguments:
@@ -133,19 +141,88 @@ def get_flatten_trial_data(h5Filename, key):
             trial = h5File["Trial{:04d}".format(i_trial)]
             for data in trial[key]:
                 keyData = np.append(keyData, data)
+    if clean:
+        keyData = clean_data(keyData, key)
     return keyData
 
 
-def frame_from_timestamp(h5Filename, timestamps):
-    frameTriggers = get_flatten_trial_data(h5Filename, "frame_triggers")
+def clean_data(data, key):
+    """Dispatch for key specific cleaning methods.
+
+    Arguments:
+        data {ndarray} -- Source data, a flattened h5 dataset.
+        key {str} -- Key describing data
+
+    Returns:
+        ndarray -- Cleaned data
+    """
+    if key == "frame_triggers":
+        data = clean_frame_trigger_data(data)
+    return data
+
+
+def analyze_frame_triggers(data):
+    """Analyze frame_trigger data for anomalies.
+
+    Arguments:
+        data {ndarray} -- Flattened frame trigger dataset.
+    """
+    print("analyze_frame_triggers NOT implemented.")
+
+
+def clean_frame_trigger_data(data, frameRate=100 / 3):
+    """Correct common frame trigger indexing errors.
+
+    Cleans mislabeled triggers, and imputes missing triggers.
+
+    Arguments:
+        data {ndarray} -- Numpy array of timestamps in ms.
+
+    Keyword Arguments:
+        frameRate {float} -- Expected distance between adjacent triggers. (default:
+            {100/3})
+
+    Returns:
+        ndarray -- Cleaned data.
+    """
+    analyze_frame_triggers(data)
+    # np.unique removes duplicates AND sorts
+    cleanData = np.unique(data)
+    interFrameIntervals = cleanData[1:] - cleanData[:-1]
+    largeGapsIndexes = (interFrameIntervals > np.ceil(frameRate)).nonzero()[0]
+    filledData = cleanData.copy()
+    # Process gaps in reverse order so inserting fillins doesn't change indexing for
+    # subsequent gap processing
+    for i_gap, gapIndex in enumerate(largeGapsIndexes[::-1]):
+        numFillIns = round(interFrameIntervals[gapIndex] / frameRate) - 1
+        fillIns = (
+            np.round((np.arange(numFillIns) + 1) * frameRate) + cleanData[gapIndex]
+        )
+        filledData = np.insert(filledData, gapIndex + 1, fillIns)
+    assert np.all(np.abs((filledData[1:] - filledData[:-1]) - frameRate) <= 1)
+    return filledData
+
+
+def frame_from_timestamp(frameTriggers, timestamps):
+    """Map timestamps to frames
+
+    Arguments:
+        frameTriggers {[long]} -- Sequence of (sorted) frame trigger timestamps.
+        timestamps {[long] or long} -- Sequence or scalar of timestamps.
+
+    Returns:
+        ndarray -- Sequence of the indexes corresponding to the first timestamp greater
+            than or equal to each element in timestamps.
+    """
     sorters = np.argsort(frameTriggers, kind="mergesort")
-    # assert all(frameTriggers == np.sort(frameTriggers))
+    assert all(frameTriggers == frameTriggers[sorters])
     return np.searchsorted(frameTriggers, timestamps, sorter=sorters)
 
 
-def process(tiffPattern, maskDir):
+def process(tiffPattern, maskDir, h5Filename):
     timeseries = open_TIFF_stack(tiffPattern)
     masks = get_masks(maskDir)
     ROIaverages = cut_to_averages(timeseries, masks)
+    frameTriggers = get_flatten_trial_data(h5Filename, "frame_triggers", clean=True)
     dF_F = get_dF_F(ROIaverages)
     return dF_F
